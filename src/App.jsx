4f3a75d37cec39cb65d93e2f9b2077ee87f89a69
Kevin Lagat <kevinlagat@Kevins-MacBook-Pro.local>
2024-08-23 00:27:48 +0300

    Set up

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./components/HomePage.jsx";
+import ImageCarousel from "./components/ImageCarousel";
+import BeamBlock from "./components/BeamBlock";
+import HollowBlocks from "./components/HollowBlocks";
+import PavingBlocks from "./components/PavingBlocks";
+import RoadKerb from "./components/RoadKerbAccessories"; // Updated import
+import Services from "./components/Services";
+import Gallery from "./components/Gallery";
+import Downloads from "./components/Downloads";
+import SignUp from "./components/SignUp";
+import "./index.css";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/beam-block" element={<BeamBlock />} />
+        <Route path="/hollow-blocks" element={<HollowBlocks />} />
+        <Route path="/paving-blocks" element={<PavingBlocks />} />
+        <Route path="/road-kerb" element={<RoadKerb />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/downloads" element={<Downloads />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+      <ImageCarousel />
+    </Router>
+  );
+}
+
+export default App;
