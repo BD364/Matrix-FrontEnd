4396e54d97bccdad4d4044e1624ea82f33b912af
deankago <martokhago@gmail.com>
2024-08-26 09:22:53 +0300

    navbar responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar/Navbar.jsx";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./components/HomePage.jsx";
 import ImageCarousel from "./components/ImageCarousel";
